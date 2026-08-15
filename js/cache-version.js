b070865e3b97cd499a6ee2f67e9eaef92f51d088
Dev.Hwang <zxc1063@naver.com>
2026-08-15 15:41:13 +0900

    20260815 Version Update(3)

--- js/cache-version.js
+++ js/cache-version.js
@@ -2,4 +2,4 @@
  * Bump SITE_ASSET_VERSION via tool/update-version.py (or edit here),
  * then HTML ?v= is applied automatically by that tool / apply-cache-bust.
  */
-window.SITE_ASSET_VERSION = "20260815152716";
+window.SITE_ASSET_VERSION = "20260815154107";
